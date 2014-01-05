3bb79f56d82cd97d4864dd0652fa1ba60c721ea4
Vincent Siliakus <vincent.siliakus@horselifestyle.tv>
2014-01-05 13:26:37 +0100

    version upped to 0.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Eml.Mixfile do
 
   def project do
     [ app: :eml,
-      version: "0.1",
+      version: "0.2",
       name: "Eml",
       source_url: "https://github.com/zambal/eml",
       elixir: "~> 0.12"

a34b902370213970df58b88e26200664237aad50
Vincent Siliakus <zambal@gmail.com>
2015-01-30 13:44:57 +0100

    Added default value for :type option in do_eml

--- lib/eml.ex
+++ lib/eml.ex
@@ -62,7 +62,7 @@ defmodule Eml do
   def do_eml(quoted \\ nil, opts) do
     (quoted || opts[:do])
     |> handle_file(opts[:file])
-    |> handle_type(opts[:use] || @default_lang, opts[:type])
+    |> handle_type(opts[:use] || @default_lang, opts[:type] || :eml)
     |> handle_precompile(opts[:env] || [], opts[:file], opts[:precompile])
     |> handle_assign(opts[:handle_assign])
   end
